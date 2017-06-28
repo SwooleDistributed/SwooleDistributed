5bbb28646642bb7910772a883160668f42ca80b9
896369042@qq.com <sdsxstbatm1991>
2017-06-28 15:13:05 +0800

    2.1.0
    composer

--- src/bin/define.php
+++ src/bin/define.php
@@ -11,14 +11,14 @@ define('MYROOT',BIN_DIR."/..");
 
 require_once MYROOT . '/vendor/autoload.php';
 
-checkfile("SERVER_DIR", MYROOT."/src/Server");
+checkfile("SERVER_DIR", MYROOT."/vendor/tmtbe/swooledistributed/src/Server");
 checkfile("APP_DIR", MYROOT . "/src/app");
 checkfile("WWW_DIR", MYROOT . "/src/www");
 checkfile("LUA_DIR", MYROOT . "/src/lua");
 checkfile("CONFIG_DIR", MYROOT . "/src/config");
 checkfile("TEST_DIR", MYROOT . "/src/test");
-checkfile("LOG_DIR", MYROOT . "/log");
-checkfile("PID_DIR", MYROOT . "/pid");
+checkfile("LOG_DIR", BIN_DIR . "/log");
+checkfile("PID_DIR", BIN_DIR . "/pid");
 
 function checkfile($name,$path)
 {
