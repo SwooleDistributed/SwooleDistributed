b79dc2aa73c9eea25e802af83e3f6316ce7703c4
896369042@qq.com <sdsxstbatm1991>
2017-09-13 14:19:29 +0800

    2.4.13

--- src/config/ports.php
+++ src/config/ports.php
@@ -9,22 +9,6 @@
 use Server\CoreBase\PortManager;
 
 $config['ports'][] = [
-    'socket_type' => PortManager::SOCK_TCP,
-    'socket_name' => '0.0.0.0',
-    'socket_port' => 9091,
-    'pack_tool' => 'LenJsonPack',
-    'route_tool' => 'NormalRoute',
-];
-
-$config['ports'][] = [
-    'socket_type' => PortManager::SOCK_TCP,
-    'socket_name' => '0.0.0.0',
-    'socket_port' => 9092,
-    'pack_tool' => 'EofJsonPack',
-    'route_tool' => 'NormalRoute',
-];
-
-$config['ports'][] = [
     'socket_type' => PortManager::SOCK_HTTP,
     'socket_name' => '0.0.0.0',
     'socket_port' => 8081,
@@ -34,10 +18,18 @@ $config['ports'][] = [
 $config['ports'][] = [
     'socket_type' => PortManager::SOCK_WS,
     'socket_name' => '0.0.0.0',
-    'socket_port' => 8083,
+    'socket_port' => 8082,
     'route_tool' => 'NormalRoute',
     'pack_tool' => 'NonJsonPack',
     'opcode' => PortManager::WEBSOCKET_OPCODE_TEXT
+];
+
+$config['ports'][] = [
+    'socket_type' => PortManager::SOCK_TCP,
+    'socket_name' => '0.0.0.0',
+    'socket_port' => 8083,
+    'pack_tool' => 'LenJsonPack',
+    'route_tool' => 'NormalRoute',
 ];
 
 return $config;
--- src/config/server.php
+++ src/config/server.php
@@ -13,17 +13,17 @@ $config['server']['send_use_task_num'] = 20;
 $config['server']['set'] = [
     'log_file' => LOG_DIR."/swoole.log",
     'log_level' => 5,
-    'reactor_num' => 2, //reactor thread num
-    'worker_num' => 4,    //worker process num
+    'reactor_num' => 1, //reactor thread num
+    'worker_num' => 1,    //worker process num
     'backlog' => 128,   //listen backlog
     'open_tcp_nodelay' => 1,
     'dispatch_mode' => 2,
-    'task_worker_num' => 5,
+    'task_worker_num' => 1,
     'task_max_request' => 5000,
     'enable_reuse_port' => true,
     'heartbeat_idle_time' => 120,//2分钟后没消息自动释放连接
     'heartbeat_check_interval' => 60,//1分钟检测一次
-    'max_connection' => 100000
+    'max_connection' => 1024
 ];
 //协程超时时间
 $config['coroution']['timerOut'] = 5000;
