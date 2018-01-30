2382ebe14a561b69aaf91e6be59981697cf0d04b
xavier <xavier007@dingtalk.com>
2018-01-30 16:12:11 +0800

    路径

--- src/bin/exec/download.sh
+++ src/bin/exec/download.sh
@@ -3,4 +3,4 @@ rm -rf consul_0.9.3_linux_amd64.zip \
 && wget -P `pwd`/bin/exec https://releases.hashicorp.com/consul/0.9.3/consul_0.9.3_linux_amd64.zip \
 && unzip `pwd`/bin/exec/consul_0.9.3_linux_amd64.zip -d `pwd`/bin/exec/ \
 && wget -p `pwd`/bin/exec https://github.com/fabiolb/fabio/releases/download/v1.5.6/fabio-1.5.6-go1.9.2-linux_amd64 \
-&& mv fabio-1.5.6-go1.9.2-linux_amd64 fabio
+&& mv `pwd`/bin/exec/fabio-1.5.6-go1.9.2-linux_amd64 `pwd`/bin/exec/fabio
