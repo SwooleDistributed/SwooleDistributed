844c73bda6c1b1fac1280c9dd8082f9b0faf6407
896369042@qq.com <sdsxstbatm1991>
2018-03-20 16:53:37 +0800

    3

--- src/config/server.php
+++ src/config/server.php
@@ -25,10 +25,7 @@ $config['server']['set'] = [
     'enable_reuse_port' => true,
     'heartbeat_idle_time' => 120,//2分钟后没消息自动释放连接
     'heartbeat_check_interval' => 60,//1分钟检测一次
-    'max_connection' => 100000,
-    'request_slowlog_timeout' => 2, //2秒
-    'request_slowlog_file' => LOG_DIR . "/trace.log",
-    'trace_event_worker' => true, //跟踪 Task 和 Worker
+    'max_connection' => 100000
 ];
 //协程超时时间
 $config['coroution']['timerOut'] = 5000;
