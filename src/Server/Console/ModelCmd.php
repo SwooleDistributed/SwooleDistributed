2b32944b203ccd46e2b0b1af10b0c142bfa45adf
896369042@qq.com <sdsxstbatm1991>
2018-05-16 19:01:53 +0800

    3.2.0

--- src/Server/Console/ModelCmd.php
+++ src/Server/Console/ModelCmd.php
@@ -3,7 +3,6 @@
 namespace Server\Console;
 
 use app\AppServer;
-use Reflection;
 use ReflectionClass;
 use ReflectionMethod;
 use Server\CoreBase\Child;
@@ -30,29 +29,76 @@ class ModelCmd extends Command
         new AppServer();
         go(function () use ($output, $input) {
             get_instance()->initAsynPools(0);
+            $save_map = [];
             $io = new SymfonyStyle($input, $output);
             while (true) {
-                $model_name = $io->ask("输入调用的Model名称");
+                while (true) {
+                    $model_name = $io->ask("输入调用的Model名称");
+                    try {
+                        $child = new Child();
+                        $model = get_instance()->loader->model($model_name, $child)->getOwn();
+                        break;
+                    } catch (\Throwable $e) {
+                        $io->error($e->getMessage());
+                    }
+                }
+                $method_name = $io->choice("输入调用的方法名", $this->getMethods(get_class($model)));
+
+                $result = $this->getFucntionParameterName(get_class($model), $method_name);
+                $params = [];
+                while (count($result) != 0) {
+                    $p = array_shift($result);
+                    while (true) {
+                        $pr = $io->ask("输入参数{$p}的值(或者输入保存的变量名以" . '$开头)');
+                        if ($this->checkSaveId($pr)) {
+                            $pr = $save_map[$pr] ?? null;
+                            if ($pr == null) {
+                                $io->error("读取变量出错");
+                            } else {
+                                break;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                    $params[] = $pr;
+                }
                 try {
-                    $child = new Child();
-                    $model = get_instance()->loader->model($model_name, $child)->getOwn();
-                    break;
+                    $class = new ReflectionClass(get_class($model));
+                    $method = $class->getMethod($method_name);
+                    $method->setAccessible(true);
+                    if ($method->isPublic()) {
+                        $result = $model->$method_name(...$params);
+                    } else {
+                        $io->note("注意：此函数是非Public函数，通过反射调用如果有协程切换不会返回结果");
+                        $result = $method->invoke($model, ...$params);
+                    }
+                    $io->text("输出结果：");
+                    print_r($result);
+                    if ($result != null) {
+                        while (true) {
+                            $save_id = $io->ask("将保存到变量，输入变量名以" . '$开头，或者Ctrl-C结束程序');
+                            if (!$this->checkSaveId($save_id)) {
+                                $io->error("无效变量名");
+                            } else {
+                                $save_map[$save_id] = $result;
+                                break;
+                            }
+                        }
+                    }
                 } catch (\Throwable $e) {
-                    $io->error($e->getMessage());
+                    print_r($e->getMessage() . "\n");
                 }
             }
-            $method_name = $io->choice("输入调用的方法名",$this->getMethods(get_class($model)));
-            //获得参数
-            $result = $this->getFucntionParameterName(get_class($model), $method_name);
-            $params = [];
-            while (count($result)!=0){
-                $p = array_shift($result);
-                $params[] = $io->ask("输入参数{$p}的值");
-            }
-            $result = $model->$method_name(...$params);
-            $io->success("结果：");
-            print_r($result);
         });
+    }
+
+    protected function checkSaveId($value)
+    {
+        if ($value[0] != "$") {
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -61,7 +107,8 @@ class ModelCmd extends Command
      * @return array
      * @throws \ReflectionException
      */
-    protected function getFucntionParameterName($class_name,$method_name) {
+    protected function getFucntionParameterName($class_name, $method_name)
+    {
         $class = new ReflectionClass($class_name);
         $method = $class->getMethod($method_name);
         $depend = array();
@@ -74,12 +121,23 @@ class ModelCmd extends Command
     /**
      * @param $class_name
      * @return array
+     * @throws \ReflectionException
      */
     protected function getMethods($class_name)
     {
-        $array1 = get_class_methods($class_name);
+        $class = new ReflectionClass($class_name);
+        $methods = $class->getMethods(ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED | ReflectionMethod::IS_PRIVATE);
+        $array1 = array();
+        foreach ($methods as $model) {
+            $array1[] = $model->name;
+        }
         if ($parent_class = get_parent_class($class_name)) {
-            $array2 = get_class_methods($parent_class);
+            $class = new ReflectionClass($parent_class);
+            $methods = $class->getMethods(ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED | ReflectionMethod::IS_PRIVATE);
+            $array2 = array();
+            foreach ($methods as $model) {
+                $array2[] = $model->name;
+            }
             $array3 = array_diff($array1, $array2);
         } else {
             $array3 = $array1;
